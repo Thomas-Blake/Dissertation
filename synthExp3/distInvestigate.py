e137ffd2f93d108a22f776c1d0f089454bef8888
Thomas Blake <tom.blakey22@gmail.com>
2022-04-01 09:17:02 +0100

    synth dist comparision

---
+++ synthExp3/distInvestigate.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+
+dist = np.load('synthExp3/dist.npy')
+dist = -dist
+dist.sort()
+dist = -dist
+ax.plot(np.arange(33), 1000*dist)
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_xlabel('Species Rank')
+ax.set_ylabel('datapoints per species')
+
+
+plt.savefig('synthExp3/images/SynthClassDist',dpi=500)
