1580b7a89f3469202572dc94975375a9b537a4f5
Thomas Blake <tom.blakey22@gmail.com>
2022-04-04 08:53:58 +0100

    Introducing Fisher Consistency experiment

---
+++ synthExp5/distributionPlot.py
@@ -0,0 +1,43 @@
+from turtle import color
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+x = np.linspace(-2.5,2.5,100)
+dist1 = (2 *np.pi)**(-0.5)*np.exp(-0.5*(x+2)**2)
+dist2 = (2 *np.pi)**(-0.5)*np.exp(-0.5*(x-2)**2)
+
+fig, ax = plt.subplots()
+
+ax.set_xticks([-2,0,2])
+
+
+ax.plot([0,0],[0,0.5],color='black',linewidth=3)
+ax.plot([0,0],[0,0.5],'--',color='orange',linewidth=3)
+
+
+ax.plot([0.25*np.log(99),0.25*np.log(99)],[0,0.5],color='grey',linewidth=3)
+
+ax.plot([0.25*np.log(1.99/1.01),0.25*np.log(1.99/1.01)],[0,0.5],color='purple',linewidth=3)
+
+#Adaptive
+ax.plot([1.08,1.08],[0,0.5],color='green',linewidth=3)
+legend1 = ax.legend(['0-1 TEST','Logit Adjusted loss','0-1 TRAIN','Equalised','Adaptive'],title="Decision Boundaries")
+ax.fill_between(x, 0, dist1,color='blue',alpha=0.3)
+ax.fill_between(x, 0, dist2,color='red',alpha=0.3)
+
+red_patch = mpatches.Patch(color='red', label='Y=2',alpha=0.3)
+blue_patch = mpatches.Patch(color='blue', label='Y=1',alpha=0.3)
+ax.legend(handles=[red_patch,blue_patch], title="Distributions")
+#ax.legend(['Y=1','Y=2'])
+plt.gca().add_artist(legend1)
+
+
+ax.set_xbound([-2.5,2.5])
+ax.set_ybound([0,0.7])
+ax.set_xlabel('x')
+plt.rcParams['text.usetex'] = True
+ax.set_ylabel('$f_{X|Y=y}(x)$')
+
+plt.savefig('synthExp5/images/dist')
